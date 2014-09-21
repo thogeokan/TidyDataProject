77678ae24393ea52a3b491182a2a95018aafb5b1
Thomas Kanold <dlonak@gmx.de>
2014-09-21 15:17:59 +0200

    making changes

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ TidyDataProject
 ===============
 
 Repository to the Tidy Data Course Project from Coursera
+
+
+## File Description
+
+### In .
+
+* README.md: This file you are reading.
+* CodeBook.md: Description of the generated datasets in data 
+* run.R: The script for 
+
+### Generated Files
+
+The script will generate the folder "data". In which it will download the Dataset.zip 
+file into. It then will unzip that archive and we have a new folder "UCI HAR Dataset".
+The script will then use the data from this folder to generate two datasets:
+
+* tidyData1.txt
+* tidyData2.txt
+
+and stores them in the data folder.
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,15 @@
 
-###
-#  
-# setwd("Documents/Programming/R/project")
+# @author: Thomas Kanold
+
+## Load the needed libraries
+library(dplyr)
+
+# Path to my PERSONAL LOKAL working directory. Left it for convenience though.
+#setwd("Documents/Programming/R/TidyDataProject")
 
 
 ###
-#  Download ZIP if not available
+#  Download ZIP
 ###
 
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -27,25 +31,33 @@ if (!file.exists(fileName)) {
 #  Load each table 
 ###
 
-datadir  <- "/Users/overmind/Documents/Programming/R/project/data/UCI HAR Dataset/"
+# UNZIP
+setwd("./data/")
+unzip("./Dataset.zip")
+setwd("..")
 
-subject_train  <- read.table(paste(datadir, "train/subject_train.txt", sep="") )
-subject_test  <- read.table(paste(datadir, "test/subject_test.txt", sep="") )
+# Load the datatables:
 
-x_train  <- read.table(paste(datadir, "train/X_train.txt", sep="") )
-x_test <- read.table(paste(datadir, "test/X_test.txt", sep="") )
+# datadir  <- "data/UCI HAR Dataset/"
 
-y_train  <- read.table(paste(datadir, "train/y_train.txt", sep="") )
-y_test <- read.table(paste(datadir, "test/y_test.txt", sep="") )
+# tbl_df is actually not needed, but the tables look nicer when you have a look at them
+subject_train  <- tbl_df(read.table("data/UCI HAR Dataset/train/subject_train.txt"))
+subject_test  <- tbl_df(read.table("data/UCI HAR Dataset/test/subject_test.txt"))
 
-act_label  <- read.table(paste(datadir, "activity_labels.txt", sep=""))
-features  <- read.table(paste(datadir, "features.txt", sep=""))
+x_train  <- tbl_df(read.table("data/UCI HAR Dataset/train/X_train.txt"))
+x_test <- tbl_df(read.table("data/UCI HAR Dataset/test/X_test.txt"))
+
+y_train  <- tbl_df(read.table("data/UCI HAR Dataset/train/y_train.txt"))
+y_test <- tbl_df(read.table("data/UCI HAR Dataset/test/y_test.txt"))
+
+act_label  <- tbl_df(read.table("data/UCI HAR Dataset/activity_labels.txt"))
+features  <- tbl_df(read.table("data/UCI HAR Dataset/features.txt"))
 
 
 ###
-#  give every column a proper name.
-#  
+#  Give every column a proper name.
 ###
+
 names(subject_train)  <- "SUBJECT_ID"
 names(subject_test)  <- "SUBJECT_ID"
 
@@ -53,78 +65,88 @@ names(subject_test)  <- "SUBJECT_ID"
 names(act_label)  <- c("ACTIVITY_ID","ACTIVITY")
 names(features)  <- c("ID","NAME")
 
-
-
-# now we give every feature its proper name
+# Give every feature column its proper name
 names(x_train)  <- features[,2]
 names(x_test)  <- features[,2]
 
 names(y_train)  <- "ACTIVITY_ID"
 names(y_test)  <- "ACTIVITY_ID"
-#
-
 
 ###
-#  Merges the training and the test sets to create one data set.
+#  Merge the training and the test sets to create one dataset.
 #
 ## STEP 1
 # 
 ###
-train  <- data.frame(y_train,subject_train,x_train)
-test  <- data.frame(y_test,subject_test,x_test)
+
+# check.names musst be false, otherwise the column names will be changed/abreviated
+train  <- data.frame(y_train,subject_train,x_train, check.names= FALSE)
+test  <- data.frame(y_test,subject_test,x_test, check.names= FALSE)
 
 big_table  <- rbind(train,test)
 
 
 ###
-#  extract only the mean and std variables. I kept the Activity and the Subject_id in there
-#  I left subject ID because it is not a measurement. 
+#  Extract only the mean and std variables. I kept the Activity and the Subject_id in there for now.
 #  
 ## STEP 2
 # 
 ###
 
-# 1 and 2 represent "ACTIVITY_ID", "SUBJECT_ID"
-# we don't want to miss them
-
+# only take the feature columns which have "mean" or "std" in their name
 sm_features_n  <- grep("mean|std", features$NAME)
 sm_feat  <- list(features[sm_features_n,2])
 
-
-
+# 1 and 2 represent "ACTIVITY_ID", "SUBJECT_ID"
 col  <- c(1,2, sm_features_n + 2)
 tidy_table  <- big_table[,col]
 
 ###
 #  Uses descriptive activity names to name the activities in the data set
 #
-## STEP 3
-# 
+## STEP 3 & STEP 4
+#
+# We took the variable except for "ACTIVITY" from features.txt
+# We could come up with newer names but this way people already familiar 
+# with the original dataset have it easier.
+#
 ###
-library(dplyr)
+
+
+###
+
 tidy_table_lab  <- inner_join(act_label, tidy_table, by = "ACTIVITY_ID",copy=FALSE)
 
-# let us get rid uf ACTIVITY_ID
-tidy_table_lab2  <- tidy_table_lab[,-1]
 
-# let us get rid of SUBJECT_ID
+# let us get rid uf ACTIVITY_ID, it is just a number
+tidy_table_lab2  <- tidy_table_lab[,-1]
+# let us get rid of SUBJECT_ID, it is just a number and according to the assignment 
+# it is not asked for in the final dataset. 
 tidy_table_lab3  <- tidy_table_lab2[,-2]
 
-###
-# STEP 4
-# we took the variable except for "ACTIVITY" from features.txt
-# we could come up with newer names but this way people already familiar 
-# with the original dataset won't have problems
-###
 
 ###
 # STEP 5
+# Summarize the ACTIVITies.
 ###
 
-library(dplyr)
-
 tt1 <- tbl_df(tidy_table_lab3)
-ttt  <- tt1 %>% group_by(ACTIVITY) %>% summarise_each(funs(mean))
+tt2  <- tt1 %>% group_by(ACTIVITY) %>% summarise_each(funs(mean))
 
+
+###
+#  Save files
+###
+# The full dataset of MEANs, STDs and ACTIVITY
+write.table(tidy_table_lab3, "data/tidyData1.txt" )
+
+# The summarized dataset of MEAN's and STD's  of each ACTIVITY
+write.table(tt2, "data/tidyData2.txt" )
+
+
+
+###
+### END
+###
 
 
